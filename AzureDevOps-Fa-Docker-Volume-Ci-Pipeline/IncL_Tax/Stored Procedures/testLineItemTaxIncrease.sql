95a6be51bc1f4ed157e4cbc5180475622b5182ad
Chris Adkin <chris@exadat.co.uk>
2019-06-03 17:59:45 +0100

    Tax unit tests changed

--- AzureDevOps-Fa-Docker-Volume-Ci-Pipeline/IncL_Tax/Stored Procedures/testLineItemTaxIncrease.sql
+++ AzureDevOps-Fa-Docker-Volume-Ci-Pipeline/IncL_Tax/Stored Procedures/testLineItemTaxIncrease.sql
@@ -9,15 +9,15 @@ BEGIN
 
 	SELECT @ExpectedSum = SUM(L_Tax + 0.01)
 	FROM   LineItem
-	WHERE  L_Tax > 0.04;
+	WHERE  L_Tax > 0.06;
 
   	UPDATE LineItem
 	SET    L_Tax = L_Tax + 0.01
-	WHERE  L_Tax > 0.04;
+	WHERE  L_Tax > 0.06;
 
 	SELECT @ActualSum = SUM(L_Tax)
 	FROM   LineItem
-	WHERE  L_Tax > 0.04;
+	WHERE  L_Tax > 0.06;
 
     EXEC tSQLt.AssertEquals @ExpectedSum , @ActualSum;
 END;
